f6fc312354f95e357056704801e4f70f89bbef6c
hirose yudai <kingkong999yhirose@gmail.com>
2016-04-15 21:51:10 +0900

    Initial Commit

---
+++ ResourceKit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ResourceKit
+//
+//  Created by kingkong999yhirose on 2016/04/15.
+//  Copyright © 2016年 kingkong999yhirose. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
