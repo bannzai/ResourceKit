6f737dcdf2ba01e012ce6801dcab4cdf6156f992
hirose yudai <kingkong999yhirose@gmail.com>
2016-05-03 12:38:18 +0900

    Add: images and parser folder for UIImage Support

---
+++ ResourceKit/ProjectParser.swift
@@ -0,0 +1,113 @@
+//
+//  ProjectParser.swift
+//  ResourceKit
+//
+//  Created by kingkong999yhirose on 2016/05/03.
+//  Copyright © 2016年 kingkong999yhirose. All rights reserved.
+//
+
+import Foundation
+
+
+struct ProjectResourceParser {
+    private var projectFile: XCProjectFile
+    var paths: [NSURL] = []
+    
+    init() {
+        self.init(xcodeURL: Environment.PROJECT_FILE_PATH.path, target: Environment.TARGET_NAME.element)
+    }
+    
+    init(xcodeURL: NSURL, target: String) {
+        guard let projectFile = try? XCProjectFile(xcodeprojURL: xcodeURL) else {
+            fatalError("xcodeURL: \(xcodeURL) ")
+        }
+        let allTarget = projectFile.project.targets
+        guard let target = allTarget.filter({ $0.name == target }).first else {
+            fatalError("allTargetName: \(allTarget.flatMap { $0.name }.joinWithSeparator(", "))")
+        }
+        
+        self.projectFile = projectFile
+        self.paths = generateFileRefPaths(target).flatMap(Environment.pathFrom)
+        
+        setupSuffixViewControllers()
+    }
+    
+    private func generateFileRefPaths(target: PBXNativeTarget) -> [Path] {
+        return target.buildPhases
+            .flatMap { $0.files }
+            .flatMap { $0.fileRef }
+            .flatMap { $0 as? PBXFileReference }
+            .flatMap { $0.fullPath }
+    }
+    
+    private mutating func setupSuffixViewControllers() {
+        let swiftViewControllerFiles = filterPaths(withExtension: "swift", suffixs: "ViewController")
+        let ObjCViewControllerFiles = filterPaths(withExtension: "h", suffixs: "ViewController")
+        
+        let viewControllerFromSwiftFile = viewControllerInfoWith(
+            swiftViewControllerFiles,
+            suffix: "ViewController",
+            pattern: ".*class\\s+.*ViewController\\s*:\\s*.*ViewController"
+        )
+        let viewControllerFromObjcFile = viewControllerInfoWith(
+            ObjCViewControllerFiles,
+            suffix: "ViewController",
+            pattern: "\\s*@interface\\s+.*ViewController\\s*:\\s*.*ViewController"
+        )
+        
+        let standards = ViewControllerResource.standards().flatMap { ViewController(className: $0.rawValue) }
+        
+        ProjectResource
+            .sharedInstance
+            .viewControllers
+            .appendContentsOf(
+                viewControllerFromSwiftFile + viewControllerFromObjcFile + standards
+        )
+    
+    }
+    
+    private func viewControllerInfoWith(path: NSURL, suffix: String, pattern: String) -> ViewController? {
+        let content = try! String(contentsOfURL: path)
+        let regex = try! NSRegularExpression(pattern: pattern, options: .UseUnixLineSeparators)
+        let results = regex.matchesInString(content, options: [], range: NSMakeRange(0, content.characters.count))
+        
+        return results.flatMap { (result) -> ViewController? in
+            if result.range.location != NSNotFound {
+                let matchingString = (content as NSString).substringWithRange(result.range) as String
+                let classes = matchingString
+                    .stringByReplacingOccurrencesOfString("\\s*@interface", withString: "", options: .RegularExpressionSearch, range: nil)
+                    .stringByReplacingOccurrencesOfString(".*class", withString: "", options: .RegularExpressionSearch, range: nil)
+                    .stringByReplacingOccurrencesOfString("{", withString: "")
+                    .stringByReplacingOccurrencesOfString("}", withString: "")
+                    .stringByReplacingOccurrencesOfString(" ", withString: "")
+                    .stringByReplacingOccurrencesOfString(":", withString: " ")
+                    .componentsSeparatedByString(" ")
+                    .filter { $0.hasSuffix(suffix) }
+                
+                return ViewController (className: classes[0], superClassName: classes[1])
+            }
+            return nil }.first
+    }
+    
+    private func viewControllerInfoWith(paths: [NSURL], suffix: String, pattern: String) -> [ViewController] {
+        return paths.flatMap {
+            viewControllerInfoWith($0, suffix: suffix, pattern: pattern)
+        }
+    }
+    
+    private mutating func filterPaths(withExtension ex: String, suffixs: String...) -> [NSURL] {
+        return paths.filter { url in
+            guard let pathExtension = url.pathExtension
+                where pathExtension == ex
+                else {
+                    return false
+            }
+            guard let fileName = url.URLByDeletingPathExtension?.lastPathComponent
+            where suffixs.contains({fileName.hasSuffix($0)})  else {
+                    return false
+            }
+            
+            return true
+        }
+    }
+}
---
+++ ResourceKit/StoryboardParser.swift
@@ -0,0 +1,120 @@
+//
+//  StoryboardParser.swift
+//  ResourceKit
+//
+//  Created by kingkong999yhirose on 2016/05/03.
+//  Copyright © 2016年 kingkong999yhirose. All rights reserved.
+//
+
+import Foundation
+
+final class StoryboardParser: NSObject, Parsable {
+    private var _name: String = ""
+    private var _initialViewControllerIdentifier: String?
+    private var _viewControllerInfos: [ViewControllerInfoOfStoryboard] = []
+    private var _currentViewControllerInfo: ViewControllerInfoOfStoryboard?
+    
+    init(url: NSURL) {
+        super.init()
+        
+        guard let ex = url.pathExtension where ex == "storyboard" else {
+            fatalError()
+        }
+        guard let name = url.URLByDeletingPathExtension?.lastPathComponent else {
+            fatalError()
+        }
+        
+        
+        _name = name
+        // Don't create ipad resources
+        if _name.containsString("~") { 
+            return
+        }
+        
+        let parser = NSXMLParser(contentsOfURL: url)!
+        parser.delegate = self
+        parser.parse()
+    }
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        if elementName == "document" {
+            _initialViewControllerIdentifier = attributeDict["initialViewController"]
+            return
+        }
+        
+        if ResourceType.isViewControllerStandardType(elementName) {
+            generateViewControllerResource(attributeDict, elementName: elementName)
+        }
+        
+        if ResourceType.isSegue(elementName) {
+            extranctionForSegue(attributeDict, elementName: elementName)
+        }
+        
+        if ResourceType.isTableViewCell(elementName) {
+            generateTableViewCells(attributeDict, elementName: elementName)
+        }
+        
+        if ResourceType.isCollectionViewCell(elementName) {
+            generateCollectionViewCells(attributeDict, elementName: elementName)
+        }
+    }
+    
+    private func extranctionForSegue(attributes: [String: String], elementName: String) {
+        guard let segueIdentifier = attributes["identifier"] else {
+            return
+        }
+        _currentViewControllerInfo?.segues.append(segueIdentifier)
+    }
+    
+    private func generateViewControllerResource(attributes: [String: String], elementName: String) {
+        guard let viewControllerId = attributes["id"] else {
+            return
+        }
+        
+        let storyboardIdentifier = attributes["storyboardIdentifier"] ?? ""
+        let viewControllerName = ResourceType(viewController: attributes["customClass"] ?? elementName).name
+        
+        _currentViewControllerInfo = ViewControllerInfoOfStoryboard (
+            viewControllerId: viewControllerId,
+            storyboardName: _name,
+            initialViewControllerId: _initialViewControllerIdentifier,
+            storyboardIdentifier: storyboardIdentifier
+        )
+        
+        ProjectResource
+            .sharedInstance
+            .viewControllers
+            .filter({ $0.name == viewControllerName })
+            .first?
+            .storyboardInfos
+            .append(_currentViewControllerInfo!)
+    }
+    
+    private func generateTableViewCells(attributes: [String: String], elementName: String) {
+        guard let reusableIdentifier = attributes["reuseIdentifier"] else {
+            return
+        }
+        
+        let className = ResourceType(reusable: attributes["customClass"] ?? elementName).name
+        ProjectResource
+            .sharedInstance
+            .appendTableViewCell(
+                className,
+                reusableIdentifier: reusableIdentifier
+        )
+    }
+    
+    private func generateCollectionViewCells(attributes: [String: String], elementName: String) {
+        guard let reusableIdentifier = attributes["reuseIdentifier"] else {
+            return
+        }
+        
+        let className = ResourceType(reusable: attributes["customClass"] ?? elementName).name
+        ProjectResource
+            .sharedInstance
+            .appendCollectionViewCell(
+                className,
+                reusableIdentifier: reusableIdentifier
+        )
+    }
+}
