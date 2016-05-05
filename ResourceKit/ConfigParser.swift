8e075b4d005fb1c68562d7ff844b488f2e41de27
hirose yudai <kingkong999yhirose@gmail.com>
2016-05-05 20:36:06 +0900

    Add: Config Parser

--- ResourceKit/ConfigParser.swift
+++ ResourceKit/ConfigParser.swift
@@ -8,21 +8,58 @@
 
 import Foundation
 
-struct ConfigParser {
+struct Config {
+    var segue: ConfigParser.Segue = ConfigParser.Segue()
+    var image: ConfigParser.Image = ConfigParser.Image()
+    var string: ConfigParser.String = ConfigParser.String()
+    var viewController: ConfigParser.ViewController = ConfigParser.ViewController()
+    var nib: ConfigParser.Nib = ConfigParser.Nib()
+    var reusable: ConfigParser.Reusable = ConfigParser.Reusable()
+    
     init() {
         if let configuPath = NSBundle.mainBundle().pathForResource("ResourceKitConfig", ofType: "plist"),
             dictionary = NSDictionary(contentsOfFile: configuPath) as? [String: AnyObject] {
+            dictionary.forEach { (key: String , value: AnyObject) in
+                switch ConfigParser.Item(rawValue: key)! {
+                case .Segue:
+                    segue = ConfigParser.Segue(value as? [String: Bool] ?? [:])
+                case .Image:
+                    image = ConfigParser.Image(value as? [String: Bool] ?? [:])
+                case .String:
+                    string = ConfigParser.String(value as? [String: Bool] ?? [:])
+                case .ViewController:
+                    viewController = ConfigParser.ViewController(value as? [String: Bool] ?? [:])
+                case .Nib:
+                    nib = ConfigParser.Nib(value as? [String: Bool] ?? [:])
+                case .Reusable:
+                    reusable = ConfigParser.Reusable(value as? [String: Bool] ?? [:])
+                }
+            }
         }
     }
 }
 
-struct Config {
+struct ConfigParser {
+    enum Item: Swift.String {
+        case Segue
+        case Image
+        case String
+        case ViewController
+        case Nib
+        case Reusable
+    }
+    
     struct Segue {
         let identifier: Bool
         let perform: Bool
         let addition: Bool
         
-        init(dictionary: [Swift.String: Bool]) {
+        init() {
+            identifier = true
+            perform = true
+            addition = true
+        }
+        init(_ dictionary: [Swift.String: Bool]) {
             identifier = dictionary["Identifier"] ?? false
             perform = dictionary["Perform"] ?? false
             addition = dictionary["Addition"] ?? false
@@ -33,15 +70,23 @@ struct Config {
         let assetCatalog: Bool
         let projectResource: Bool
         
-        init(dictionary: [Swift.String: Bool]) {
+        init() {
+            assetCatalog = true
+            projectResource = true
+        }
+        init(_ dictionary: [Swift.String: Bool]) {
             assetCatalog = dictionary["AssetCatalog"] ?? false
             projectResource = dictionary["ProjectResource"] ?? false
         }
     }
+    
     struct String {
         let localized: Bool
         
-        init(dictionary: [Swift.String: Bool]) {
+        init() {
+            localized = true
+        }
+        init(_ dictionary: [Swift.String: Bool]) {
             localized = dictionary["Localized"] ?? false
         }
     }
@@ -49,7 +94,10 @@ struct Config {
     struct ViewController {
         let instantiateViewController: Bool
         
-        init(dictionary: [Swift.String: Bool]) {
+        init() {
+            instantiateViewController = true
+        }
+        init(_ dictionary: [Swift.String: Bool]) {
             instantiateViewController = dictionary["InstantiateViewController"] ?? false
         }
     }
@@ -57,7 +105,10 @@ struct Config {
     struct Nib {
         let xib: Bool
         
-        init(dictionary: [Swift.String: Bool]) {
+        init() {
+            xib = true
+        }
+        init(_ dictionary: [Swift.String: Bool]) {
             xib = dictionary["Xib"] ?? false
         }
     }
@@ -65,7 +116,10 @@ struct Config {
     struct Reusable {
         let identifier: Bool
         
-        init(dictionary: [Swift.String: Bool]) {
+        init() {
+            identifier = true
+        }
+        init(_ dictionary: [Swift.String: Bool]) {
             identifier = dictionary["Identifier"] ?? false
         }
     }
