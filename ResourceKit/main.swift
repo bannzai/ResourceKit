d82b36165388b700e14955c94e97fd8ba6b4db16
Hirose.Yudai <hirose.yudai@moneyforward.co.jp>
2016-05-11 12:10:07 +0900

    Revert "DEBUG"
    This reverts commit a0a7e1a56794ea6ee0939ed000d9125c4e1cafbf.

--- ResourceKit/main.swift
+++ ResourceKit/main.swift
@@ -11,10 +11,6 @@ private let RESOURCE_FILENAME = "Resource.generated.swift"
 private let outputPath = NSProcessInfo.processInfo().environment["SRCROOT"]!
 private let outputUrl = NSURL(fileURLWithPath: outputPath)
 
-// DEBUG
-private var debug = ""
-private var debugURL = outputUrl.URLByAppendingPathComponent("ResourceKit.log.swift", isDirectory: false)
-
 private var resourceValue: AnyObject?
 try! outputUrl.getResourceValue(&resourceValue, forKey: NSURLIsDirectoryKey)
 
@@ -187,4 +183,3 @@ func write(code: String, fileURL: NSURL) {
 }
 
 write(content, fileURL: writeUrl)
-write(debug, fileURL: debugURL)
