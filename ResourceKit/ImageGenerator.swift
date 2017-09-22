f15f8d2cfaa35846c76e11a560b7c8fef3ac11b3
bannzai <kingkong999yhirose@gmail.com>
2017-09-22 23:10:50 +0900

    :recycle: remove ImageGenerator

--- ResourceKit/ImageGenerator.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ImageGenerator.swift
-//  ResourceKit
-//
-//  Created by kingkong999yhirose on 2016/05/03.
-//  Copyright © 2016年 kingkong999yhirose. All rights reserved.
-//
-
-import Foundation
