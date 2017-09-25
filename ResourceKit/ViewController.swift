df4a9a5599205e50744b2c773683f7afe5920f40
bannzai <kingkong999yhirose@gmail.com>
2017-09-25 12:09:20 +0900

    :bug: should to reference type for append segue

--- ResourceKit/ViewController.swift
+++ ResourceKit/ViewController.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct ViewControllerInfoOfStoryboard {
+class ViewControllerInfoOfStoryboard {
     let viewControllerId: String
     let storyboardName: String
     let initialViewControllerId: String?
